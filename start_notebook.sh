0ee403275ad18194e1aa6a2670757cb3f9aa05a7
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-05-07 12:06:52 +0200

    initial commit

---
+++ start_notebook.sh
@@ -0,0 +1,5 @@
+#!/home/anglistik/robertsw/local-clou/bin/bash
+
+cd /home/anglistik/robertsw/clou-notebooks
+export LD_LIBRARY_PATH=/usr/localshare/intel/composer_xe_2011_sp1.6.233/mkl/lib/intel64:/usr/localshare/intel/composer_xe_2011_sp1.6.233/compiler/lib/intel64
+/home/anglistik/robertsw/local-clou/bin/ipython notebook --no-browser
