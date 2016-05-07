ad7864671e7e9906572f5334b9fc88fb8885f7d3
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-05-07 12:16:34 +0200

    tunnel_clou.sh: start browser?

--- tunnel_clou.sh
+++ tunnel_clou.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 PORT=8888
+(sleep 3 && open "http://localhost:$PORT") &
 ssh -t -L localhost:$PORT:localhost:$PORT amor ssh -t -L localhost:$PORT:localhost:8888 clou clou-notebooks/start_notebook.sh
