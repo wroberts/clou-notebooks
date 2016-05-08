2b0007bd70db16b2d7187c49ea8a213cecf780a5
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-05-08 15:55:07 +0200

    tunnel_clou.sh: keep trying

--- tunnel_clou.sh
+++ tunnel_clou.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 PORT=8888
-(sleep 3 && open "http://localhost:$PORT") &
+(sleep 3 ; open "http://localhost:$PORT") &
 ssh -t -L localhost:$PORT:localhost:$PORT amor ssh -t -L localhost:$PORT:localhost:8888 clou clou-notebooks/start_notebook.sh
+wait
